bf2dfa36166e6cd771355cb9dd2f59eee4144ea2
Vlasceanu Mihai <you@example.com>
2021-05-23 16:17:30 +0300

    Make the call to /authentication for login token, save the token in sessionStorage and create authenticated routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
 import "./App.css";
-import LoginComponent from "./components/loginComponent";
+import LoginComponent from "./components/login/LoginComponent";
+import AuthenticatedRoute from "./auth/AuthenticatedRoute";
 
-function App() {
+const App = withRouter(({ location }) => {
   return (
-    <LoginComponent/>
+    <div className="App">
+      <Switch>
+        {/* TODO: De adaugat /404 -> ErrorComponent */}
+        <Route>
+          {location.pathname !== `/login` &&
+            location.pathname !== `/signin` &&
+            location.pathname !== `/` &&
+            location.pathname}
+          <Switch>
+            <Route path="/login" exact component={LoginComponent} />
+            <Route path="/signin" exact component={LoginComponent} />
+            <Route path="/" exact component={LoginComponent} />
+            {/* TODO: Aici vin AuthenticatedRoute */}
+          </Switch>
+        </Route>
+      </Switch>
+    </div>
   );
-}
+  // return <LoginComponent />;
+});
 
 export default App;
---
+++ src/auth/AuthenticatedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AuthenticationService from "./AuthenticationService";
+import {Route, Redirect} from "react-router-dom";
+
+function AuthenticatedRoute(props){
+    const {isUserLoggedIn} = AuthenticationService();
+
+    if(isUserLoggedIn){
+        return <Route {...props}/>;
+    }else{
+        return <Redirect to = "/login"/>;
+    }
+}   
+
+export default AuthenticatedRoute;
