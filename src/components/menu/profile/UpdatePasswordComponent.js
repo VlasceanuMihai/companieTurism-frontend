946ac1f877b5adb59a1a2072b0820cc24fccf5c2
Vlad Alexandru Popa <vladap1994@gmail.com>
2021-08-18 02:30:00 +0300

    mda

--- src/components/menu/profile/UpdatePasswordComponent.js
+++ src/components/menu/profile/UpdatePasswordComponent.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { setProfile } from "../../../redux/actions/actions";
 import clsx from "clsx";
 import {
   Box,
@@ -10,129 +11,212 @@ import {
   CardHeader,
   Divider,
   TextField,
-} from "@material-ui/core";
+} from "@material-ui/core/";
 import EmployeeService from "../../../services/EmployeeService";
 
 const useStyles = makeStyles({
   root: {
     width: "400px",
     position: "relative",
-    top: "50px",
+    top: '50px',
+    margin:"0px 20px 0px 20px"
   },
-  paper: {
-    display: "flex",
-    justifyContent: "center",
+  paper:{
+    display:'flex',
+    justifyContent:'center',
   },
-  cardStyle: {
-    background: "linear-gradient(120deg, #BFADA9, #F0E2DD 40%, #ffffff)",
+  cardStyle:{
+    background: "linear-gradient(120deg, #BFADA9, #F0E2DD 40%, #ffffff)"
   },
-  btnStyle: {
-    background: "linear-gradient(45deg, #F1CDB9 10%, #b6aeab 90%)",
+  btnStyle:{
+    background: "linear-gradient(45deg, #F1CDB9 10%, #b6aeab 90%)"
   },
-  inputColor: {
-    backgroundColor: "white",
-  },
+  inputColor:{
+    backgroundColor:"white",
+  }
 });
 
 function UpdatePasswordComponent() {
   const classes = useStyles();
-  const { updatePassword } = EmployeeService();
+  const { getProfile } = EmployeeService();
+  const [profileError, setProfileError] = useState(null);
+  const [profileData, setProfileData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    cnp: "",
+    dateOfEmployment: "",
+    employeeType: "",
+  });
   const [passwordData, setPasswordData] = useState({
     currentPassword: "",
     newPassword: "",
-    confirmNewPassword: "",
   });
 
   function handleChange(event) {
-    event.preventDefault();
-
     setPasswordData({
       ...passwordData,
-      [event.target.name]: event.target.value,
+      [event.target.name]: [event.target.value],
     });
   }
 
   function handleSubmit(event) {
-    event.preventDefault();
+    setPasswordData({});
+  }
 
-    updatePassword({
-      currentPassword: passwordData.currentPassword,
-      newPassword: passwordData.newPassword,
-      confirmNewPassword: passwordData.confirmNewPassword,
-    })
+  useEffect(() => {
+    getProfile()
       .then((response) => {
-        if (response.status === 200) {
-          console.log("Password updated!");
-        }
+        console.log("Profile" + response.data);
+        setProfile({
+          firstName: response.data.firstName,
+          lastName: response.data.lastName,
+          email: response.data.email,
+          phoneNumber: response.data.phoneNumber,
+          cnp: response.data.cnp,
+          dateOfEmployment: response.data.dateOfEmployment,
+          employeeType: response.data.employeeType,
+        });
       })
       .catch((error) => {
         console.log(error);
       });
-
-    setPasswordData({
-      currentPassword: "",
-      newPassword: "",
-      confirmNewPassword: "",
-    });
-  }
+  }, []);
 
   return (
     <paper className={classes.paper}>
-      <form className={clsx(classes.root)}>
-        <Card className={classes.cardStyle}>
-          <CardHeader title="Actualizare parola" />
-          <Divider />
-          <CardContent>
-            <TextField
-              className={classes.inputColor}
-              fullWidth
-              id="currentPassword"
-              name="currentPassword"
-              type="input"
-              label="Parola curenta"
-              margin="normal"
-              variant="outlined"
-              onChange={handleChange}
-              value={passwordData.currentPassword || ""}
-            />
-            <TextField
-              className={classes.inputColor}
-              fullWidth
-              id="newPassword"
-              name="newPassword"
-              type="input"
-              label="Parola noua"
-              margin="normal"
-              variant="outlined"
-              onChange={handleChange}
-              value={passwordData.newPassword || ""}
-            />
-            <TextField
-              className={classes.inputColor}
-              fullWidth
-              id="confirmNewPassword"
-              name="confirmNewPassword"
-              type="input"
-              label="Confirmare parola noua"
-              margin="normal"
-              variant="outlined"
-              onChange={handleChange}
-              value={passwordData.confirmNewPassword || ""}
-            />
-          </CardContent>
-          <Divider />
-          <Box display="flex" justifyContent="flex-end" p={2}>
-            <Button
-              className={classes.btnStyle}
-              color="primary"
-              variant="contained"
-              onClick={handleSubmit}
-            >
-              Actualizare
-            </Button>
-          </Box>
-        </Card>
-      </form>
+    <div>
+    <form className={classes.root}>
+      <Card className={classes.cardStyle}>
+        <CardHeader title="Profil" />
+        <Divider />
+        <CardContent>
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="Nume"
+          variant="outlined"
+          margin="normal"
+          type="input"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="Prenume"
+          variant="outlined"
+          margin="normal"
+          type="input"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          type="email"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="Telefon"
+          variant="outlined"
+          margin="normal"
+          type="tel" pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="CNP"
+          variant="outlined"
+          margin="normal"
+          type="number" 
+          maxlength="13"
+          minlength="13"
+          oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          variant="outlined"
+          margin="normal"
+          type="date"
+          />
+          <TextField 
+          className={classes.inputColor}
+          fullWidth
+          id="outlined-basic"
+          label="Tip angajat"
+          variant="outlined"
+          margin="normal"
+          type="input"
+          />
+        </CardContent>
+        <Divider />
+        <Box display="flex" justifyContent="flex-end" p={2}>
+          <Button className={classes.btnStyle} color="primary" variant="contained" onClick={handleSubmit}>
+            Confirm
+          </Button>
+        </Box>
+      </Card>
+    </form>
+    </div>
+    <form className={clsx(classes.root)} >
+      <Card className={classes.cardStyle}>
+        <CardHeader title="Change Password" />
+        <Divider />
+        <CardContent>
+          <TextField
+            className={classes.inputColor}
+            fullWidth
+            id="oldPassword"
+            name="oldPassword"
+            type="input"
+            label="Parola Curenta"
+            margin="normal"
+            variant="outlined"
+            onChange={handleChange}
+            value={passwordData.password || ""}
+          />
+          <TextField
+            className={classes.inputColor}
+            fullWidth
+            id="newPassword"
+            name="newPassword"
+            type="input"
+            label="Parola Noua"
+            margin="normal"
+            variant="outlined"
+            onChange={handleChange}
+            value={passwordData.password || ""}
+          />
+          <TextField
+            className={classes.inputColor}
+            fullWidth
+            id="confirmPassword"
+            name="confirmPassword"
+            type="input"
+            label="Confirmare Parola"
+            margin="normal"
+            variant="outlined"
+            onChange={handleChange}
+            value={passwordData.confirmPassword || ""}
+          />
+        </CardContent>
+        <Divider />
+        <Box display="flex" justifyContent="flex-end" p={2}>
+          <Button className={classes.btnStyle} color="primary" variant="contained" onClick={handleSubmit}>
+            Confirm
+          </Button>
+        </Box>
+      </Card>
+    </form>
     </paper>
   );
 }
